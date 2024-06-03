a14db6b9032df2b745b0bb29d79661bdd85e92a1
PLThabangR <rakgoropthabang@gmail.com>
2024-06-03 13:40:16 +0200

    Event handling in react using inline functions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home'
 
+
+const BtnFunction =()=>{
+  const clickHandler = () => console.log("Clicked")
+  return (
+    <button onClick={()=> console.log("Inline function")}>Click me</button>
+  )
+}
 function App() {
  
   return (
     <div className="App">
       <Home/>
+      <BtnFunction/>
     </div>
   );
 }
